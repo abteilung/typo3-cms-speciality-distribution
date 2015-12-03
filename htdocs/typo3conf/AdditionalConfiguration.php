8cdfa374bee191e63cc6e5f1f45faafa211fab00
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-12-03 19:09:44 +0100

    [TASK] Compatibility TYPO3 7 LTS
    Resolves #123
    Resolves #122

--- htdocs/typo3conf/AdditionalConfiguration.php
+++ htdocs/typo3conf/AdditionalConfiguration.php
@@ -1,28 +1,22 @@
 <?php
-
 // Security tip for production: putting the database password outside the document root!
 $databaseCredentialsFile = PATH_site . '../configuration/Settings.php';
 if (file_exists($databaseCredentialsFile)) {
 	require_once ($databaseCredentialsFile);
-
 	# file ``configuration/Settings.php`` should contains something like:
 	# Database settings
 	//$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = '';
 	//$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = '';
 	//$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = '';
 	//$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = '';
-
 	# Backend settings
 	//$GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = '';
 }
-
 # Frontend Settings
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling'] = '/404/';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageUnavailable_handling'] = '/503/';
-
 // Declare Content Rendering for the Frontend
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array('fluidcontentcore/Configuration/TypoScript/');
-
 // Add some more prefix table for the live search (the top right search)
 // Usage for editor: #user:foo
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['user'] = 'fe_users';
@@ -33,7 +27,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['be_user'] = 'be_users';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['be_group'] = 'be_groups';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['address'] = 'tt_address';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['news'] = 'news';
-
 // Add configuration for Development Context
 if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isDevelopment()) {
 	$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = '1';
@@ -46,3 +39,4 @@ if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isDevelopme
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = '28674';
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = '1';
 }
+$GLOBALS['TYPO3_CONF_VARS']['FE']['activateContentAdapter'] = 0;
